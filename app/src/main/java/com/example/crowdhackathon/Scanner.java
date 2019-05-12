67b206f23b34cad20c21abdf4cc6911fa7a3ce1b
Nikolaos Katsilidis <katsilidisn@gmail.com>
2019-05-12 11:07:34 +0300

    transaction with blockchain completed

--- app/src/main/java/com/example/crowdhackathon/Scanner.java
+++ app/src/main/java/com/example/crowdhackathon/Scanner.java
@@ -2,14 +2,26 @@ package com.example.crowdhackathon;
 
 import android.app.Activity;
 import android.content.Intent;
+import android.os.AsyncTask;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.Toast;
 
 import com.google.zxing.integration.android.IntentIntegrator;
 import com.google.zxing.integration.android.IntentResult;
+
+import org.json.JSONObject;
+
+import java.io.BufferedOutputStream;
+import java.io.BufferedWriter;
+import java.io.DataOutputStream;
+import java.io.OutputStream;
+import java.io.OutputStreamWriter;
+import java.net.HttpURLConnection;
+import java.net.URL;
 
 public class Scanner extends AppCompatActivity {
     private Button scan_btn;
@@ -41,10 +53,52 @@ public class Scanner extends AppCompatActivity {
             }
             else{
                 Toast.makeText(this , result.getContents(), Toast.LENGTH_LONG).show();
+                sendPost();
             }
         }
         else{
             super.onActivityResult(requestCode , resultCode , data);
         }
+    }
+
+
+    public void sendPost() {
+        Thread thread = new Thread(new Runnable() {
+            @Override
+            public void run() {
+                try {
+                    URL url = new URL("http://172.16.220.105:3000/api/Trade?$class=org.example.mynetwork.Trade&commodity=resource:org.example.mynetwork.Commodity&newOwner=resource:org.example.mynetwork.Trader");
+                    HttpURLConnection conn = (HttpURLConnection) url.openConnection();
+                    conn.setRequestMethod("POST");
+                    conn.setRequestProperty("Content-Type", "application/json;charset=UTF-8");
+                    conn.setRequestProperty("Accept","application/json");
+                    conn.setDoOutput(true);
+                    conn.setDoInput(true);
+
+                    JSONObject jsonParam = new JSONObject();
+                    jsonParam.put("$class", "org.example.mynetwork.Trade");
+                    jsonParam.put("commodity","resource:org.example.mynetwork.Commodity#RevVolut");
+                    jsonParam.put("newOwner", "resource:org.example.mynetwork.Trader#TRADER1");
+
+
+                    Log.i("JSON", jsonParam.toString());
+                    DataOutputStream os = new DataOutputStream(conn.getOutputStream());
+                    //os.writeBytes(URLEncoder.encode(jsonParam.toString(), "UTF-8"));
+                    os.writeBytes(jsonParam.toString());
+
+                    os.flush();
+                    os.close();
+
+                    Log.i("STATUS", String.valueOf(conn.getResponseCode()));
+                    Log.i("MSG" , conn.getResponseMessage());
+
+                    conn.disconnect();
+                } catch (Exception e) {
+                    e.printStackTrace();
+                }
+            }
+        });
+
+        thread.start();
     }
 }
